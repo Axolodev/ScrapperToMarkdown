f11621d790d135bc486e09e3a4a6da9e81950400
Roberto Ruiz <rroberto.rruiz@gmail.com>
2017-04-13 10:31:56 -0500

    File processsing

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.crawler import CrawlerProcess, CrawlerRunner
 from twisted.internet import defer, reactor
 from bs4 import BeautifulSoup
+from shutil import copyfile
 import logging
 import sys
 import re
 import scrapy
+import os
 
 # Filesystem: https://learnpythonthehardway.org/book/ex16.html
 #
@@ -14,14 +16,18 @@ logging.getLogger('scrapy').propagate = False
 pages = []
 activity_urls = []
 
+# Scrapea la pagina principal. Obtiene todos los URLs de todas las actividades.
 class MainSpider(scrapy.Spider):
     name = "Main_Spider"
     start_urls = ["http://127.0.0.1:8080/"]
 
     def parse(self, response):
         global activity_urls
+        # Obtener todos los links
         links = response.xpath("//body//a/@href").extract()
         for link in links:
+            # Verificar si el link que se obtuvo es una actividad. La pagina
+            # principal tiene muchos links, por lo que es necesario filtrarlos
             result = re.match(r"MP?(\d+\.\d+(\.\d+)?)\.html?", link)
             if result is not None:
                 activity_urls.append("http://127.0.0.1:8080/" + link)
@@ -35,14 +41,22 @@ class ActivitySpider(scrapy.Spider):
     def parse(self, response):
         global activity_urls
         global pages
+        # La pagina tendra estructura de diccionario
         page = {}
-        print("Scrapping url:", response.url)
+        print("Scraping url:", response.url)
         page["doc_name"] = response.url.replace("http://127.0.0.1:8080/", "")
+        # Sacar el body del html
         page_body = response.xpath("//body").extract_first()
         try:
+            # Hacer que BeautifulSoup parsee el html del body
             soup = BeautifulSoup(page_body)
+            # Quitar los scripts del texto
             soup.body.script.decompose()
+            # Obtener texto limpio
             page["html"] = soup.get_text()
+            # Obtener titulo
+            page["title"] = response.xpath("//title").extract_first().encode("utf-8")
+            # Obtener imagenes
             page["images"] = response.xpath("//img/@src").extract()
         except Exception as e:
             print(e)
@@ -50,7 +64,17 @@ class ActivitySpider(scrapy.Spider):
 
 runner = CrawlerRunner()
 
-@defer.inlineCallbacks # https://hackedbellini.org/development/writing-asynchronous-python-code-with-twisted-using-inlinecallbacks/
+# El Scraping de las paginas se ejecuta en dos etapas:
+# 1. Scraping de la pagina principal, obtiene todos los URLs de las actividades
+#    a las que se les hara scraping en la segunda etapa.
+# 2. Scraping de las actividades. Obtiene el contenido especifico de cada
+#    actividad individual, esto es: imagenes y texto de la pagina.
+# Este proceso genera una lista de paginas de cada una de las actividades, la
+# cual se procesa mas abajo.
+# Para poder hacer scraping en dos procesos separados, se utilizan
+# inlineCallbacks. Para leer mas sobre esto, visitar el siguiente link:
+# https://hackedbellini.org/development/writing-asynchronous-python-code-with-twisted-using-inlinecallbacks/
+@defer.inlineCallbacks
 def crawl():
     yield runner.crawl(MainSpider)
     yield runner.crawl(ActivitySpider, activity_urls)
@@ -61,8 +85,52 @@ reactor.run()
 
 print("Done. Starting doc-gen")
 for page in pages:
-    page["html"] = re.sub(r"\r|\xa0|\t", "", page["html"])
-    page["html"] = re.sub(r"[ ]+", " ", page["html"])
-    # page["html"] = page["html"].replace("\n", "").replace("\r", "")
-    print(page)
-    sys.exit()
+    print("Processing " + page["doc_name"])
+    # Carpeta donde se guardaran los archivos
+    root_folder = "pug_files/"
+    # Nombre del archivo que se trabajara en esta iteracion
+    filename = page["doc_name"].replace(".html", "").replace(".htm", "")
+    # Carpeta donde se guardaran los resultados de esta iteracion
+    content_output_path = root_folder + "content/" + filename + "/"
+    if not os.path.exists(content_output_path):
+        os.makedirs(content_output_path)
+    # Archivo de salida del codigo pug
+    pug_file = open(root_folder + filename + ".pug", "w+");
+    # Archivo de salida de texto en formato markdown
+    markdown_file = open(content_output_path + filename + ".md", "w+")
+
+    # Limpiar el texto un poco mas
+    try:
+        page["html"] = re.sub(r"\r|\t", "", page["html"])
+        page["html"] = re.sub(r"[ ]+", " ", page["html"])
+        # Codificar el html a un formato procesable por python antes de imprimirlo
+        page["html"] = page["html"].encode("utf-8")
+
+        markdown_file.write(page["html"])
+    except Exception as e:
+        print("ERROR " + page["doc_name"])
+        print(page)
+        continue
+
+    if page.get("images", None) is not None:
+        for image in page["images"]:
+            # Copiar las imagenes
+            image = image.encode("utf-8")
+            markdown_file.write("[![](" + content_output_path + image + ")]\n")
+            try:
+                copyfile("/home/robruizr/Documents/moondoreyes.com/public_html/" + image, content_output_path + image)
+            except Exception as e:
+                pass
+
+    pug_content = """
+    extends ../templates/activity_template.pug
+    prepend title
+        | {0}
+
+    block activity_content
+        .mdl-grid
+            .mdl-layout-spacer
+                include:markdown-it {1}"""
+    pug_content = pug_content.format(page.get("title", page["doc_name"]), content_output_path + filename + ".md")
+
+    pug_file.write(pug_content)
